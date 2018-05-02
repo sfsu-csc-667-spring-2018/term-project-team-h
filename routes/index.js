b0037d8c91d0eca0a188c4b2855baa1313e24748
arthemg <arthemg@gmail.com>
2018-05-01 19:02:08 -0700

    socket.io chat

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.sendFile(__dirname + '/index.ejs');
   res.render('index', { title: 'Login' });
 });
 
