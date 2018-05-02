b220732a89a5409a81fc457d29e806a88a63c619
westune <moiseslorenzo13@gmail.com>
2018-05-01 19:12:50 -0700

    Game Page Skeleton

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var game = require('./routes/game');
 var db = require('./db/DbInit');
 
 var app = express();
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/game', game)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
