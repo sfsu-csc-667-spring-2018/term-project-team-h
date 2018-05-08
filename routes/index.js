65edf300707c8b21e95b0cd42b19f5c903c055be
westune <moiseslorenzo13@gmail.com>
2018-05-07 17:02:08 -0700

    Game Stuff

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile(__dirname + '/index.ejs');
+router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Login' });
 });
 
