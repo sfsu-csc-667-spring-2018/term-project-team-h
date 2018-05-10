534dd4327705e9fb3769375394d258fb47fe6275
Ralph <acosta.ralph.d@gmail.com>
2018-05-09 17:30:48 -0700

    Created migrations for games, cards, gameCards, & gameUsers tables

---
+++ migrations/20180510001341-cards.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('cards', {
+      cardId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        unique: true
+      },
+      cardNumber: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      cardSuit: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      cardValue: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+    });
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('cards');
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
