e4fc501c611d3c67b138117e310c970c40620b29
westune <moiseslorenzo13@gmail.com>
2018-05-11 13:57:06 -0700

    yes

--- sockets/index.js
+++ sockets/index.js
@@ -7,48 +7,74 @@ const io = socket_io();
 
 // socketAPI.io = io;
 
-var players = [], index = 0;
+//key: table number value: array of players
+let tables = new Map();
+//
 
 io.on('connection', function(socket){
 	console.log('A User Connected');
-	
+
 	socket.on('chat message', function(msg){
 		console.log('message: ' + msg);
 		io.emit('chat message', msg)
 	});
 
-	socket.on('disconnect', function(){
+
+	socket.on('disconnect', function(data){
+		//remove player from table
 		console.log('A User Disconnected');
+		io.emit(data.table, {player: data.player, action: "disconnect"});
 	});
 
+
 	socket.on('bet', function(data){
-		io.emit('bet', data);
-        index = (index + 1) % players.length;
-        io.emit('setTurn', {player: players[index], amount: data.amount});
+		//TODO: update pot
+		//		update nextPlayer
+		//		send card if necessary
+		let nextPlayer;
+		io.emit(data.table, {action: "bet", amount: data.amount, player: data.player, nextPlayer: nextPlayer});
 	});
 
 	socket.on('fold', function (data){
-		io.emit('fold', data);
-        index = (index + 1) % players.length;
-        io.emit('setTurn', {player: players[index], amount: 0});
+		//TODO: take player out of rotation
+		//		check if there is a winner, award the winner, then reset the game
+		//		update player Turn
+		//		update pot if necessary
+		//
+        io.emit(data.table, {action: "fold", player: data.player});
+
     });
 
 	socket.on('call', function(data){
-		index = (index + 1) % players.length;
-		io.emit('call', {player: data.player, amount: 10});
-        io.emit('setTurn', {player: players[index], amount: 0});
+		/*TODO: update nextPlayer
+		* 		update pot
+		* 		update player's bank
+		* 		check if a card needs to be sent
+		* */
+		let nextPlayer;
+        io.emit(data.table, {action: "call", player: data.player, amount: data.amount, nextPlayer: nextPlayer});
 	});
 
 	socket.on('new player', function(data){
-		players.push(data);
-		io.emit(data, {playerIndex: players.length - 1, player: players[index], amount: 0});
-        io.emit('setTurn', {player: players[index], amount: 0});
-        io.emit('users', players);
+		//TODO: add player to db
+		const t = data.table;
+
+		if(tables.get(t) == undefined){
+			tables.set(t, [data.player]);
+		}else{
+
+            console.log(tables.get(t));
+			tables.set(t, tables.get(t).push(data.player));
+		}
+
+		io.emit(data.table, {player: data.player, action: 'new user', seat: tables.get(data.table).length - 1, table: tables.get(data.table)});
 	});
 
-	socket.on('check', function(){
-		io.emit('setTurn', {player: players[0], amount: 0});
+	socket.on('check', function(data){
+		io.emit(data.table, {player: player, action: 'check'});
 	});
+
+
 
 
 });
