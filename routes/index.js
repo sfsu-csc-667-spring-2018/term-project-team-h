8c686e3106799b9b147c7126b94fab294ede0d13
westune <moiseslorenzo13@gmail.com>
2018-05-12 22:00:15 -0700

    Stuff

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Login' });
 });
+
 
 module.exports = router;
